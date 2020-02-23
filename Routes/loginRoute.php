eed2895aab0af92b70fe1430c40fd3f0103b09c3
pateras95 <kostasmc2@gmail.com>
2020-02-24 01:45:39 +0200

    small changes on from styling

--- Routes/loginRoute.php
+++ Routes/loginRoute.php
@@ -12,37 +12,42 @@ include('../API/login.php');
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
         <style type="text/css">
             .wrapper{ 
-                width: 500px;
+                max-width: 500px;
+                min-width: 500px;
                 padding: 20px; 
-                margin:0 auto;
                 margin-top: 250px;
                 border:1px solid black;
                 border-radius: 10px;
-                }
-        </style>
-        </head>
-        <body>
+            }
 
-            <div class="row">
-                <div class="wrapper">
-                    <h2>Login</h2>
-                    <p>Please fill in your credentials to login.</p>
-                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-                        <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
-                            <label>Username</label>
-                            <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
-                            <span class="help-block"><?php echo $username_err; ?></span>
-                        </div>    
-                        <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
-                            <label>Password</label>
-                            <input type="password" name="password" class="form-control">
-                            <span class="help-block"><?php echo $password_err; ?></span>
-                        </div>
-                        <div class="form-group">
-                            <input type="submit" class="btn btn-primary" value="Login">
-                        </div>
-                        <p>Don't have an account? <a href="./registerRoute.php">Sign up now</a>.</p>
-                    </form>
+            .title-text{
+                font-family: Arial, sans-serif;
+                font-size: 25px;
+                margin:0 auto;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="row justify-content-center">
+            <div class="col-lg-4 wrapper ">
+                <span class="title-text">Login</span>
+                <p>Please fill in your credentials to login.</p>
+                <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                    <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+                        <label>Username</label>
+                        <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
+                        <span class="help-block"><?php echo $username_err; ?></span>
+                    </div>
+                    <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+                        <label>Password</label>
+                        <input type="password" name="password" class="form-control">
+                        <span class="help-block"><?php echo $password_err; ?></span>
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" class="btn btn-primary" value="Login">
+                    </div>
+                    <p>Don't have an account? <a href="./registerRoute.php">Sign up now</a>.</p>
+                </form>
             </div>
         </div>
     </body>
